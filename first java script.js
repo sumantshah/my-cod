fe3ad3f6b106dbc4a87d018450d11468837d8406
sumantshah <sumantshah95583728@gmail.com>
2024-08-08 19:26:57 +0530

    this is my first js code

---
+++ first java script.js
@@ -0,0 +1,131 @@
+ /*const profile = {
+ username : " sumant shah",
+flowers: 197,
+follwing:60,
+post:8,
+}
+console.log(profile);
+// artimetic operstors
+let a = 5;
+let b = 2;
+ 
+console.log (" a = ", a, "& b =" ,b);
+console.log ("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log (" a * b =", a * b);
+console.log ("a / b =", a / b);
+console.log ("a % b =", a % b );
+
+
+//Unary operator
+{
+    let a = 6;
+    let b = 5;
+     console.log("a = ", a & "b = ", b)
+     a = a+1; // 7
+     console.log ("a = ", a); 
+} */
+// asignment operator
+ 
+/*let a = 8;
+let b = 5; 
+a += 5
+console.log("a = ", a)*/
+
+    // comparision operators
+    // let a = 5;
+    // let b = 6;
+    // console.log("a == b", a == b);
+
+     // logical operators
+    //  let a = 5;
+    //  let b = 7;
+      
+    //  let cond1 = a < b;
+    //  let cond2 = a ===b;
+    //  console.log("cond1 && cond2 =", a < b && a === b);
+
+    // conditional statements
+    /* let age = 25;
+
+    if ( age >= 18 ) {
+         console.log("you can vote");
+     }
+    
+         if ( age < 18 ) {
+             console.log(" you can note vote ");
+         } 
+    
+        let mode  = "dark";
+        let color;
+        if (mode === "dark") {
+            color = "black";
+        }
+        if ( mode === "light") {
+            color = "white"
+        }
+        console.log(color); */
+
+        // 
+        
+        // let num = 12;
+        // if ( num % 2 ===0 ){
+        //     console.log("even");
+        // } else {
+        //     console.log ("odd")
+        // }
+        /*let mode = "dark";
+        let color;
+        if ( mode === "dark") {
+            color = "black";
+        } else if ( mode === "blue") {
+            color = "blue";
+        } else if ( mode === "pink") {
+            color = "pink";
+        } else {
+            color = 'white';
+        }
+        console.log(color);*/
+
+/*let age = 25;
+ let result = age >= 18 ? "adult" : "not adult"
+console.log(result); */
+    //   let num = prompt("enter a number: ");
+    //   if (num % 5 === 0) {
+    //     console
+    //     .log ( num, "is a multiple of 5");
+    //   } else {
+    //     console.log (num, "is note a multiple of 5");
+    //   }
+
+    /*let scores = 87;
+    let grade;
+    
+    if ( score >= 90 && score <= 100) {
+        grade = "A";
+    } else if (score >= 70 && csore <= 89){
+        grade = "B";
+    } else if (score >= 60 && score <= 69){
+        grade = "C";
+    } else if ( score >= 50 && score <=59){
+        grade = "D";
+    } else if (score >=0 && dcore <= 49){
+        grade = "F";
+    }
+    console.log( " according to yuor score, your grade was:", grade);*/
+    let scores = 87;
+let grade;
+
+if (scores >= 90 && scores <= 100) {
+    grade = "A";
+} else if (scores >= 70 && scores <= 89) {
+    grade = "B";
+} else if (scores >= 60 && scores <= 69) {
+    grade = "C";
+} else if (scores >= 50 && scores <= 59) {
+    grade = "D";
+} else if (scores >= 0 && scores <= 49) {
+    grade = "F";
+}
+
+console.log("According to your score, your grade is:", grade);
